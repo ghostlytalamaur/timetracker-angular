7efb061115c83f25ef363afe4219e1d49b161629
Vladimir MIkhalenko <mihalenko.w.a@gmail.com>
2021-04-27 00:37:46 +0300

    update eslint config

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['../../.eslintrc.js'],
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'dist'],
   overrides: [
     {
       files: ['*.ts'],
