722dc32f2e0eac5c31073b49f7279e433eab2b10
Vladimir Mikhalenko <mikhalenko@acdlabs.by>
2022-12-03 20:26:50 +0300

    feat: show the end time in the sessions table

--- apps/fire-tracker/src/app/sessions/sessions-table/sessions-table.component.ts
+++ apps/fire-tracker/src/app/sessions/sessions-table/sessions-table.component.ts
@@ -7,9 +7,19 @@ import { formatDuration } from '../../utils/format-duration';
   name: 'ttSessionDuration',
   standalone: true,
 })
-export class SessionDurationPipePipe implements PipeTransform {
+export class SessionDurationPipe implements PipeTransform {
   transform(value: Session): string {
     return value.durationMs > 0 ? formatDuration(value.durationMs) : '';
+  }
+}
+
+@Pipe({
+  name: 'ttSessionEnd',
+  standalone: true,
+})
+export class SessionEndPipe implements PipeTransform {
+  transform(value: Session): Date {
+    return new Date(+value.start + value.durationMs);
   }
 }
 
@@ -19,7 +29,7 @@ export class SessionDurationPipePipe implements PipeTransform {
   styleUrls: ['./sessions-table.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, SessionDurationPipePipe],
+  imports: [CommonModule, SessionDurationPipe, SessionEndPipe],
 })
 export class SessionsTableComponent {
   @Input()
