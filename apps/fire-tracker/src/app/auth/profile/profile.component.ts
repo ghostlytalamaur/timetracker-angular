0a937ea4e72f3af3daab36bc20964c1743ae22c6
Vladimir Mikhalenko <mikhalenko@acdlabs.by>
2022-12-07 21:41:36 +0300

    feat: update icons

--- apps/fire-tracker/src/app/auth/profile/profile.component.ts
+++ apps/fire-tracker/src/app/auth/profile/profile.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { PushModule } from '@ngrx/component';
 import { Store } from '@ngrx/store';
+import { IconComponent } from '../../ui/icon.component';
 import { authActions, authFeature } from '../auth.store';
 
 @Component({
@@ -10,7 +10,7 @@ import { authActions, authFeature } from '../auth.store';
   templateUrl: './profile.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [CommonModule, PushModule, MatIconModule],
+  imports: [CommonModule, PushModule, IconComponent],
 })
 export class ProfileComponent {
   private readonly store = inject(Store);
--- apps/fire-tracker/src/app/header/user-button/user-button.component.ts
+++ apps/fire-tracker/src/app/header/user-button/user-button.component.ts
@@ -4,13 +4,14 @@ import { authActions, authFeature } from '../../auth/auth.store';
 import { PushModule } from '@ngrx/component';
 import { NgIf } from '@angular/common';
 import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
+import { IconComponent } from '../../ui/icon.component';
 
 @Component({
   selector: 'tt-user-button',
   templateUrl: './user-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [PushModule, NgIf, CdkMenuTrigger, CdkMenu, CdkMenuItem],
+  imports: [PushModule, NgIf, CdkMenuTrigger, CdkMenu, CdkMenuItem, IconComponent],
 })
 export class UserButtonComponent {
   private readonly store = inject(Store);
